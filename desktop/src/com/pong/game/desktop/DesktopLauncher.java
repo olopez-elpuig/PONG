a9e9eccd1d03d23df31c17c3aca63e4eb15f5d9c
Oscar <olopez@elpuig.xeill.net>
2020-04-25 17:39:55 +0200

    Initial commit

---
+++ desktop/src/com/pong/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pong.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pong.game.PONG;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PONG(), config);
+	}
+}
