a8921dbbcc96bd7ead1b4c8885ee4422a312f44d
Oscar <olopez@elpuig.xeill.net>
2020-04-25 18:57:29 +0200

    new assets added

--- core/src/com/pong/game/PONG.java
+++ core/src/com/pong/game/PONG.java
@@ -6,14 +6,18 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+//640 x 480
+
 public class PONG extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
+	float pressX , pressY;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("Start.png");
 	}
 
 	@Override
@@ -21,13 +25,7 @@ public class PONG extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, pressX, pressY);
 		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
