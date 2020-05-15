c0a07c70a187d238d3917e54b1ebe407100dcfe7
Oscar <olopez@elpuig.xeill.net>
2020-05-15 04:06:40 +0200

    new assets added abd override

--- core/src/com/pong/game/PONG.java
+++ core/src/com/pong/game/PONG.java
@@ -5,19 +5,25 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import javafx.scene.shape.Rectangle;
+
+import java.awt.*;
 
 //640 x 480
 
 public class PONG extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private static Texture img;
+	private static SpriteBatch batch;
 
-	float pressX , pressY;
+	float bolaX , bolaY;
 
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("Start.png");
+		img = new Texture("bolaPrincipal.png");
+
+		int bolaX = 220;
+		int bolaY = 210;
 	}
 
 	@Override
@@ -25,7 +31,27 @@ public class PONG extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, pressX, pressY);
+		batch.draw(img, bolaX, bolaY);
 		batch.end();
+	}
+
+	@Override
+	public void mover(render){
+		int dx=1;
+		int dy=1;
+		bolaX+=dx;
+		bolaY+=dy;
+		if(bolaX>render.getMaxX()){
+			dx=-dx;
+		}
+		if(bolaY>render.getMaxY()){
+			dy=-dy;
+		}
+		if(bolaX<0){
+			dx=-dx;
+		}
+		if(bolaY<0){
+			dy=-dy;
+		}
 	}
 }
